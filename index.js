c6ced0478816772149555cf24876436c153d1049
ReD <foo@bar.com>
2024-10-21 17:54:21 +0400

    add start logging

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const BOT_TOKEN=(process.env.BOT_TOKEN || "test");
 
 (async ()=>{
     try {
+        console.log(`Application started. Version: ${process.env.VERSION}.`);
         const data = readFileSync("./config/config.json", 'utf8');
         let config = JSON.parse(data);
         let bot = new Bot(BOT_TOKEN,config);
         await bot.initCommands();
-        await bot.start();
     } catch (e) {
-        if (e.code === 'ENOENT' || e.code === 'ENOENT') {
+        if (e.code === 'ENOENT' || e.code === 'EACCES') {
             console.error(`Error on reading config: ${e.message}`);
         }
         else if(e.name==='GrammyError'){
